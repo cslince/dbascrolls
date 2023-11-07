79d3a7a8cb53d0832373685c22ef09c2f55fc7e5
cslince <88627058+cslince@users.noreply.github.com>
2023-11-07 11:57:49 +0530

    Update missing Index Suggestions.sql

--- missing Index Suggestions.sql
+++ missing Index Suggestions.sql
@@ -1,48 +1,13 @@
-Sure, I can help you with that. To retrieve missing index details with query id in T-SQL, you can use the following script:
-
-```sql
 SELECT 
-    migs.avg_total_user_cost,
-    migs.avg_user_impact,
-    migs.user_seeks,
-    migs.user_scans,
-    migs.last_user_seek,
-    migs.last_user_scan,
-    migs.avg_total_logical_reads,
-    migs.avg_total_logical_writes,
-    migs.last_logical_reads,
-    migs.last_logical_writes,
-    migs.database_id,
-    mid.database_id,
-    mid.[object_id],
-    mid.statement,
-    mid.equality_columns,
-    mid.inequality_columns,
-    mid.included_columns,
-    mid.unique_compiles,
-    mid.user_seeks,
-    mid.user_scans,
-    mid.last_user_seek,
-    mid.last_user_scan,
-    mid.system_seeks,
-    mid.system_scans,
-    mid.last_system_seek,
-    mid.last_system_scan,
-    mid.avg_total_user_cost,
-    mid.avg_total_logical_reads,
-    mid.avg_total_logical_writes,
-    migs.unique_compiles,
-    migs.last_compile_start,
-    migs.last_compile_end,
-    migs.avg_compile_duration,
-    migs.query_id,
-    migs.query_plan_hash
-FROM 
-    sys.dm_db_missing_index_group_stats AS migs
-INNER JOIN 
-    sys.dm_db_missing_index_groups AS mig ON migs.group_handle = mig.index_group_handle
-INNER JOIN 
-    sys.dm_db_missing_index_details AS mid ON mig.index_handle = mid.index_handle
-```
-
-This script retrieves information about missing indexes along with details like query id. Keep in mind that this script is just a starting point and may need to be adjusted depending on your specific requirements or environment.
+  qs.query_id,
+  migs.avg_total_user_cost,
+  migs.avg_user_impact,
+  -- other columns...
+FROM sys.dm_db_missing_index_groups mig  
+INNER JOIN sys.dm_db_missing_index_group_stats migs 
+  ON migs.group_handle = mig.index_group_handle
+INNER JOIN sys.dm_db_missing_index_details mid
+  ON mig.index_handle = mid.index_handle
+INNER JOIN sys.dm_exec_query_stats qs
+  ON qs.sql_handle = mid.sql_handle
+ORDER BY migs.avg_total_user_cost DESC, migs.avg_user_impact DESC
