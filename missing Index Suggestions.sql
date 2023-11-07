e0e6d13fcdd59a6cd5dfa374a7edf565bd9d7dae
cslince <88627058+cslince@users.noreply.github.com>
2023-11-07 12:09:52 +0530

    Update missing Index Suggestions.sql

--- missing Index Suggestions.sql
+++ missing Index Suggestions.sql
@@ -11,3 +11,31 @@ INNER JOIN sys.dm_db_missing_index_details mid
 INNER JOIN sys.dm_exec_query_stats qs
   ON qs.sql_handle = mid.sql_handle
 ORDER BY migs.avg_total_user_cost DESC, migs.avg_user_impact DESC
+
+
+Certainly! To add a query ID to the code, you would first need a way to generate unique IDs for each query. Since SQL Server does not have a built-in query ID system, you would need to implement your own. One way to do this is by using a combination of the database ID (dbid), object ID (objectid), and execution count.
+
+Here's an example of how you can modify your query to include a query ID:
+
+```sql
+SELECT
+    qs_cpu.total_worker_time / 1000 AS total_cpu_time_ms,
+    CONCAT(q.dbid, '_', q.objectid, '_', qs_cpu.execution_count) AS query_id,
+    q.[text],
+    p.query_plan,
+    qs_cpu.execution_count,
+    q.dbid,
+    q.objectid,
+    q.encrypted AS text_encrypted
+FROM
+    (SELECT TOP 500 qs.plan_handle,
+     qs.total_worker_time,
+     qs.execution_count FROM sys.dm_exec_query_stats qs ORDER BY qs.total_worker_time DESC) AS qs_cpu
+CROSS APPLY sys.dm_exec_sql_text(plan_handle) AS q
+CROSS APPLY sys.dm_exec_query_plan(plan_handle) p
+WHERE p.query_plan.exist('declare namespace 
+        qplan = "http://schemas.microsoft.com/sqlserver/2004/07/showplan";
+        //qplan:MissingIndexes')=1
+```
+
+In this modified code, I've added a `CONCAT` function to generate a unique query ID using the `dbid`, `objectid`, and `execution_count`. This should help identify each query uniquely. Keep in mind that this method assumes that the combination of these three values will be unique for each query in your database. If this assumption doesn't hold, you may need to implement a more sophisticated query ID generation logic.
